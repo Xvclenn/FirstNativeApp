39c3e84f20cb5272ac8a1ddf8cb73ad3ec160f9a
Xvclenn <https://github.com/Xvclenn>
2023-05-30 20:06:26 +0800

    FirstCommit

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>I LOVE WIJJFLY ❤️</Text>
-      <Text>SO MUCH ❤️</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default App = () => {
+	return (
+		<View style={styles.container}>
+			<View style={styles.top}>
+				<TextInput style={styles.input} placeholder="Add notes" />
+				<Button style={styles.button} title="hi" />
+			</View>
+			<View style={styles.middle}>
+				<Text>hi</Text>
+			</View>
+			<View style={styles.bottom} />
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		justifyContent: "space-between",
+		backgroundColor: "#fff",
+		padding: 20,
+		marginTop: 50,
+		marginBottom: 50,
+	},
+	top: {
+		flex: 0.2,
+		flexDirection: "row",
+		backgroundColor: "grey",
+		borderWidth: 5,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+	},
+	middle: {
+		flex: 0.2,
+		backgroundColor: "beige",
+		borderWidth: 5,
+	},
+	bottom: {
+		flex: 0.2,
+		backgroundColor: "pink",
+		borderWidth: 5,
+		borderBottomLeftRadius: 20,
+		borderBottomRightRadius: 20,
+	},
+
+	input: {},
+	button: {},
 });
